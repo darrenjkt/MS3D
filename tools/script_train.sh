66fb672d26d3387f69cf2ec5119e22006c31c07b
Darren <darrenjktsai@gmail.com>
2023-04-11 14:56:55 +1000

    updated configs

--- tools/script_train.sh
+++ tools/script_train.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-pid=2106
-tail --pid=$pid -f /dev/null
+python train.py --cfg_file cfgs/nuscenes_models/uda_pv_rcnn_plusplus_resnet_anchorhead.yaml \
+                --extra_tag 10sweep_xyzt_allcls
 
-python train.py --cfg_file cfgs/nuscenes_models/uda_voxel_rcnn_centerhead.yaml \
-                --extra_tag 1sweep_xyz
+python train.py --cfg_file cfgs/nuscenes_models/uda_pv_rcnn_plusplus_resnet_anchorhead.yaml \
+                --extra_tag 1sweep_xyz_allcls --set DATA_CONFIG.MAX_SWEEPS 1 DATA_CONFIG.POINT_FEATURE_ENCODING.used_feature_list 'x','y','z'
