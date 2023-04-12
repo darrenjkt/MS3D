f76131805d09fbed4a50635b47b9cc55c814a75e
Darren Tsai <39115809+darrenjkt@users.noreply.github.com>
2023-04-12 15:30:51 +1000

    Delete script_train.sh

--- tools/script_train.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-python train.py --cfg_file cfgs/nuscenes_models/uda_pv_rcnn_plusplus_resnet_anchorhead.yaml \
-                --extra_tag 10sweep_xyzt_allcls
-
-python train.py --cfg_file cfgs/nuscenes_models/uda_pv_rcnn_plusplus_resnet_anchorhead.yaml \
-                --extra_tag 1sweep_xyz_allcls --set DATA_CONFIG.MAX_SWEEPS 1 DATA_CONFIG.POINT_FEATURE_ENCODING.used_feature_list 'x','y','z'
